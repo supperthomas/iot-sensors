efbb1dcdb0746298e09a69c6137e993707bee693
Adrian Sychowiec <adrian.sychowiec@ssginsight.com>
2018-11-16 20:24:38 +0100

    use comon wifi and mqtt  modules

--- ping-mqtt/config.lua
+++ ping-mqtt/config.lua
@@ -1,21 +1,17 @@
 -- conf module
-
-local M = {}
-
-M.WIFI = { 
-	ssid = "NetworkName", 
-	pwd = "Password",
-	save = false,
-	auto = true
-}
-
-M.MQTT = {
-	HOST = "192.168.1.2",
-	PORT = 1883,
-	KEEPALIVE = 120,
-	ID = node.chipid(),
-	ENDPOINT = "/NodeMCU/",
-	DEVPATH = "/NodeMCU/" .. node.chipid()
-}
-
-return M
+return(
+	WIFI = { 
+		ssid = "NetworkName", 
+		pwd = "Password",
+		save = false,
+		auto = true
+	},
+	MQTT = {
+		HOST = "192.168.1.2",
+		PORT = 1883,
+		KEEPALIVE = 120,
+		ID = node.chipid(),
+		ENDPOINT = "/NodeMCU/",
+		DEVPATH = "/NodeMCU/" .. node.chipid()
+	}
+)
