f488ae2f9cb7f9874a4a22ecf3b5c9f71047c395
Adrian Sychowiec <adrian.sychowiec@ssginsight.com>
2018-11-19 19:38:54 +0100

    separate humidity module replace wifi code

---
+++ humidity-syhs200-mqtt/mod_syhs200_mqtt.lua
@@ -0,0 +1,47 @@
+local M = {}
+local mqttHnd = nil
+local pubTimer = tmr.create()
+local cfg = {
+    PUBDELAY = 10000
+}
+
+local function initADC()
+    if adc.force_init_mode(adc.INIT_ADC)
+    then
+        print("Initializing ADC, restarting...")
+        node.restart()
+    end
+end
+
+local function publishHumidity()
+    print("*** publish humidity syhs200 event ***")
+
+	local radc = adc.read(0)
+	local v = 2.0*radc/666.0
+	local h = v*50.0/1.650
+
+    print("/adc: " .. radc)
+	print("/adc/volt: " .. v)
+	print("/adc/humidity: " .. h)
+    
+	mqttHnd.publish("/adc", radc ,0,0)
+	mqttHnd.publish("/adc/volt", v ,0,0)
+	mqttHnd.publish("/adc/humidity", h ,0,0)
+end
+
+function M.initialize()
+    initADC()
+end
+
+function M.start(mqttHandler, sensorConfig)
+    mqttHnd = mqttHandler
+    cfg = sensorConfig
+    pubTimer:alarm(cfg.PUBDELAY, tmr.ALARM_AUTO, publishHumidity)
+    publishHumidity()
+end
+
+function M.stop()
+    pubTimer:unregister()
+end
+
+return M
---
+++ humidity-syhs200-mqtt/mod_wifi.lua
@@ -0,0 +1,41 @@
+--wifi module
+
+local M = {}
+local conTmr = tmr.create()
+local connectedCbk = nil
+
+function M.onConnected(connectedCallback)
+	connectedCbk = connectedCallback
+end
+
+function M.stop()
+	conTmr:unregister()
+end
+
+function M.connect(wifiConfig)
+	wifi.setmode(wifi.STATION)
+	tmr.delay(1000)
+	local it = 0
+	wifi.sta.config(wifiConfig)
+	print("Trying connect to " .. wifiConfig.ssid .. ":")
+	conTmr:alarm(1000, tmr.ALARM_AUTO, function()   
+		it=it+1
+		ip, nm, gw=wifi.sta.getip()
+		if ip ~= nil then
+			conTmr:unregister()         
+			print("IP Address: ",ip,"\nNetmask: ",nm,"\nGateway: ",gw)         
+			
+			if connectedCbk ~= nil then
+				connectedCbk();
+			end
+			return
+		end
+		if it >= 180 then
+			print("Unable connect to network, restarting...")
+			node.restart()
+		end
+		print(".")
+	end )
+end
+
+return M
